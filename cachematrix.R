6e853aa4c64b4b74df9dbd7443ddd8a16fe236b9
arousos <angelo.rousos@gmail.com>
2015-03-22 10:08:32 -0400

    cachematrix.R Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow a matrix to be stored when 
+## solved for inverse if not already stored.
 
-## Write a short comment describing this function
+## makeCacheMatrix stores a matrix for later access.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  storedMatrix <- NULL  
+  
+  set <- function(y) {
+    x <<- y
+    storedMatrix <<- NULL
+  }
+  
+  get <- function(y) {
+    return (x)
+  }
+  
+  setMatrix <- function(newMatrix){
+    storedMatrix <<- newMatrix
+  }
+  
+  getMatrix <- function() {
+    return(storedMatrix)
+  }
+  
+  
+  list(set = set, get = get,
+       setMatrix = setMatrix,
+       getMatrix = getMatrix)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve looks for a matrix stored with makeCacheMatrix and then inverts it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getMatrix()
+  if(!is.null(m)) {
+    message("Getting cached Matrix")
+    return(m)
+  }
+  
+  data <- x$get() 
+  m <- solve(data, ...)
+  x$setMatrix(m)
+  m
+  
 }
